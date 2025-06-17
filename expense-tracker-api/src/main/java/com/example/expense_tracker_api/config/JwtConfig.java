36b4c965218b3520353ec9028063b984dc8f9930
Ahmad Hafizh Assaad <71488@fif.co.id>
2025-06-17 14:31:52 +0700

    :white_check_mark: Add feature/create-model-and-dto

---
+++ expense-tracker-api/src/main/java/com/example/expense_tracker_api/config/JwtConfig.java
@@ -0,0 +1,4 @@
+package com.example.expense_tracker_api.config;
+
+public class JwtConfig {
+}
--- expense-tracker-api/src/main/java/com/example/expense_tracker_api/model/Expense.java
+++ expense-tracker-api/src/main/java/com/example/expense_tracker_api/model/Expense.java
@@ -1,5 +1,30 @@
 package com.example.expense_tracker_api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "expenses")
+@Data
 public class Expense {
-    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    @JsonProperty("user_id")
+    private Long userId;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(name = "category_id", nullable = false)
+    @JsonProperty("category_id")
+    private Long categoryId;
+
+    private String note;
 }
